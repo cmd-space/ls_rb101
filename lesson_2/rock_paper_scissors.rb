370645092eb95668db28b93726054ba4aa740216
Mason Crane <masonecrane@gmail.com>
2020-05-19 12:51:09 -0500

    added spock and lizard as choices, converted choice, and tweaked logic

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-VALID_CHOICES = %w[rock paper scissors].freeze
+VALID_CHOICES = %w[rock r paper p scissors sc lizard l spock sp].freeze
 
 def prompt(message)
   puts "=> #{message}"
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  (first == 'rock' && %w[scissors lizard].include?(second)) ||
+    (first == 'paper' && %w[rock spock].include?(second)) ||
+    (first == 'scissors' && %w[paper lizard].include?(second)) ||
+    (first == 'lizard' && %w[spock paper].include?(second)) ||
+    (first == 'spock' && %w[scissors rock].include?(second))
 end
 
 def display_result(player, computer)
@@ -22,18 +24,32 @@ def display_result(player, computer)
   end
 end
 
+def convert_choice!(choice)
+  case choice
+  when 'r' then choice = 'rock'
+  when 'p' then choice = 'paper'
+  when 'sc' then choice = 'scissors'
+  when 'l' then choice = 'lizard'
+  when 'sp' then choice = 'spock'
+  else choice
+  end
+  choice
+end
+
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
+    prompt('Choose from the following')
+    prompt('The option to the right of each choice can be used for ease')
+    prompt("#{VALID_CHOICES.join(', ')}")
+    choice = convert_choice!(gets.chomp)
 
     break if VALID_CHOICES.include?(choice)
 
     prompt('That is not a valid choice. Please try again.')
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = convert_choice!(VALID_CHOICES.sample)
 
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
